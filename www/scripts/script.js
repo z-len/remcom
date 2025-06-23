7973ca7ca9ec7c35fd040989cc070f2f36261719
Elena <z-len@yandex.ru>
2025-06-23 14:01:36 +0300

    lesson 23/06/2025

--- www/scripts/script.js
+++ www/scripts/script.js
@@ -1,1 +1,11 @@
 console.log('script work!');
+
+let burger = document.querySelector('.js-burger');
+
+burger.addEventListener('click', function(event) {
+    event.preventDefault();
+    console.log('burger');
+    let menu = document.querySelector('.js-menu');
+    menu.classList.add('open');
+})
+
