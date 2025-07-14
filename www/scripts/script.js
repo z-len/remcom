31248a3976ff11a157097aaad75acb97a2115f36
Elena <z-len@yandex.ru>
2025-07-14 16:52:21 +0300

    14/07/2025

--- www/scripts/script.js
+++ www/scripts/script.js
@@ -1,11 +1,31 @@
 console.log('script work!');
 
 let burger = document.querySelector('.js-burger');
+let menu = document.querySelector('.js-menu');
 
 burger.addEventListener('click', function(event) {
     event.preventDefault();
-    console.log('burger');
-    let menu = document.querySelector('.js-menu');
+    if(menu.classList.contains('open')){
+        menu.classList.remove('open');
+        return
+    }
     menu.classList.add('open');
 })
 
+
+// let btn = document.querySelector('.js-button');
+// let box = document.querySelector('.js-box');
+// // let isGreen = true;
+
+// btn.addEventListener('click', function() {
+//   if (box.classList.contains('blue')) {
+//     box.classList.remove('blue');
+//     // box.classList.add('blue');
+//     // isGreen = false;
+//     return;
+//   }
+
+//   box.classList.add('blue');
+//   // box.classList.remove('blue');
+//   // isGreen = true;
+// });
